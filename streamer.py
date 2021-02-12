d3191ddfb4e6926e454656043bb5321772b50d13
joannayu98 <joannayu2021@u.northwestern.edu>
2021-02-11 20:08:36 -0600

    work for part 3.b, accounting for acks. Not working yet

--- streamer.py
+++ streamer.py
@@ -26,13 +26,14 @@ class Streamer:
         #send & receive buffers
         #self.s_buff = {}
         self.r_buff = {}
+        self.s_buff = {}
 
         self.closed = False
 
-        #creating an executor with max (1) thread
+        #creating an executor with max (2) thread
         executor = ThreadPoolExecutor(max_workers=1)
-        #submits a task and gets a future, allows us to ask later if the task is resolved
-        future = executor.submit(self.listener)
+        #submits a task
+        executor.submit(self.listener)
         print("created executor!")
 
     def send(self, data_bytes: bytes) -> None:
@@ -43,30 +44,33 @@ class Streamer:
             header = pack('i', self.seq_num)
             sbytes = header + data_bytes[0:1468]
             self.socket.sendto(sbytes, (self.dst_ip, self.dst_port))
+            self.s_buff[self.seq_num] = sbytes
             data_bytes = data_bytes[1468:]
 
             self.seq_num = self.seq_num + 1
-
             #self.s_buff[self.seq_num] = sbytes
 
         header = pack('i', self.seq_num)
+        #print(header)
         sbytes = header + data_bytes
         #print(sbytes)
         self.socket.sendto(sbytes, (self.dst_ip, self.dst_port))
+        self.s_buff[self.seq_num] = sbytes
         self.seq_num = self.seq_num + 1
+        #print('packet num check', self.seq_num, data_bytes[0:1468], 'stored in sbytes', sbytes)
+
 
 
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
         # your code goes here!  The code below should be changed!
         while True:
-
             #can be while loop too
             if self.recv_num in self.r_buff:
-
+                #print('IN OUR RECV FUNC', self.recv_num, 'CURR R_BUFF IS:', self.r_buff)
                 data = self.r_buff[self.recv_num]
-                # dont technically need this
-                # del self.r_buff[self.recv_num]
+                #cleans up the r_buff after packet is sent to instance 1
+                del self.r_buff[self.recv_num]
                 self.recv_num += 1
 
                 return data
@@ -76,26 +80,38 @@ class Streamer:
             # #print(recv_header)
             # data = data[4:]
             # self.r_buff[recv_header] = data
-            # print(data, 'recv function-----------------------')
             #print(self.r_buff)
 
     def listener(self):
         #code taken from the project page so far
         while not self.closed:
             try:
-                data, addr = self.socket.recvfrom(1472)
+                data, addr = self.socket.recvfrom()
                 recv_header = unpack('i', data[0:4])[0]
-                #print(recv_header)
+                #print('HEADER IS', recv_header)
                 data = data[4:]
-                self.r_buff[recv_header] = data
-                print(data, 'listener -----------------------')
+                #print('DATA IS', data)
+                #self.r_buff[recv_header] = data
                 # print(addr)
                 #r_buff{data} = data
+
+                #check if ACK or data
+                if recv_header > 0:
+                    #print('NEW DATA IS', data)
+                    self.r_buff[recv_header] = data
+                    #print('RBUFF IS', self.r_buff)
+
+                elif recv_header < 0:
+                    check_header = recv_header * -1
+                    if check_header in self.s_buffer:
+                        self.s_buffer.pop(check_header)
+
+
             except Exception as e:
                 print("listener died, uh o!")
                 print(e)
 
-        print(future.done())
+
 
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
